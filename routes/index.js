63b85743a24d7c49a61a524c835d3ca0bf4839df
Mitchell <46569264+MGSawatsky@users.noreply.github.com>
2019-03-18 19:02:20 -0600

    Saves Created Routes
    This will now save the created route in the SaveRoutes.JSON file.
    
    I set this up in the same way I setup the chat assignment so it might be a little janky.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
+
+var htmlPath = path.join(__dirname);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+app.get('/', function(req, res){
+  app.use(express.static(htmlPath));
+  res.sendFile('D:\\GitHub\\Carpul\\public\\index.html');
+});
+
+
+io.on('connection', function(socket){
+
+  socket.on('getMapObject', function(directionsObj){
+    console.log(JSON.stringify(directionsObj.origin) + " " + JSON.stringify(directionsObj.destination) + " " + directionsObj.travelMode);
+    var fs = require('fs');
+    fs.appendFile("SavedRoutes.JSON", JSON.stringify(directionsObj)+"\n", function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
+  });
+});
+
 module.exports = router;
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
