5f8c93699bc0b8ba78551c92989681c3699adfd3
s-seguin <stuart.seguin@ucalgary.ca>
2019-04-05 15:33:12 -0600

    display warning messages to client on failed log in

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,7 @@ var router = express.Router();
 module.exports = function (passport) {
 
     router.get('/', function(req, res, next) {
-        res.render('../public/login.ejs', {title: 'Login', message:''});
+        res.render('../public/login.ejs', {title: 'Login', message:req.flash('login')});
     });
 
     router.post('/', passport.authenticate('local-login', {
