174dc087163a06b123fea18315fe6a903c0fef91
s-seguin <stuart.seguin@ucalgary.ca>
2019-04-10 11:25:55 -0600

    initial insert of ride into database in index.js -need to fix TODO -rideObj needs user id -need to use real value

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,60 @@
 var express = require('express');
 var router = express.Router();
-//var passport = require('passport');
-//var dbConn = require('../config/database');
-//var pg = require('pg');
-/*var path = require('path');
-var app = express();
-var http = require('http').Server(app);
-var port = process.env.PORT || 3000; */
 
+//db stuff
+var dbConn = require('../config/database');
+var pg = require('pg');
+var dbClient;
 
+if (dbConn.ssl == "true") {
+  console.log("[DB]  connecting to heroku db");
+  dbClient = new pg.Client(
+      {
+        connectionString: dbConn.herokuConn,
+        ssl: dbConn.ssl
+      }
+  );
+} else {
+  console.log("[DB]  connecting to local db");
+  dbClient = new pg.Client(dbConn.localConn);
+}
+
+dbClient.connect();
+
+/***
+ * Insert a new ride into the Ride table
+ * @param rideObj
+ */
+function insertIntoDB(rideObj) {
+  //TODO: Remove hard coded values and use real value from users
+  console.log("Insert into DB");
+  dbClient.query(
+      "INSERT INTO ride(user_id, start_location, end_location, expire, capacity, available, embedded_map, directions_obj, ride_date, created_on, price_per_seat) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, Now(), Now(), $9);",
+      [2, 'Calgary', 'Edmonton', rideObj.expire, rideObj.capacity, rideObj.capacity, rideObj.embeddedMapString, JSON.stringify(rideObj.directionsObj), '4'],
+      (err, res) => {
+        if (res) {
+          console.log(res);
+        } else {
+          console.log("There was an error inserting user into database " + err);
+        }
+      }
+  );
+  //dbClient.close();
+}
+
+///TESTING to make sure the thing inserted
+function selectAllFromRide() {
+  dbClient.query(
+      "select * from ride",
+      (err, res) => {
+        if (res) {
+          res.rows.forEach((item) => console.log(item));
+        } else {
+          console.log("There was an error inserting user into database " + err);
+        }
+      }
+  );
+}
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
@@ -21,6 +67,7 @@ function isLoggedIn(req, res, next) {
   res.redirect('/login');
 }
 
+
 module.exports = function(passport, server) {
 
   var savedUsername = null;
@@ -31,6 +78,7 @@ module.exports = function(passport, server) {
   });
 
   router.get('/', isLoggedIn, function(req, res, next) {
+    selectAllFromRide();
     console.log('index.js get');
     savedUsername = req.user.fname;
     console.log('Cookie already has value: ' + req.cookies['Carpul'] + 'Setting to ' + savedUsername);
@@ -77,6 +125,7 @@ module.exports = function(passport, server) {
     var fs = require('fs');
 
     socket.on('sendNewMapToServer', function(directionsObj){
+      //TODO: get user id from session and store in rideObj
       console.log(JSON.stringify(directionsObj.origin) + " " + JSON.stringify(directionsObj.destination) + " " + directionsObj.travelMode);
       var rideObj = new Object();
       rideObj.driver = socket.username; //value got from register
@@ -88,6 +137,8 @@ module.exports = function(passport, server) {
       rideObj.directionsObj = directionsObj; //sending theh whole thing cause why not
       //Send this map right back to the client
       //// TODO: Insert this into the database
+      insertIntoDB(rideObj);
+
       console.log("sending: " + JSON.stringify(rideObj) );
       socket.emit('sendEmbeddedMap', rideObj);
     });
@@ -117,3 +168,9 @@ module.exports = function(passport, server) {
 
   return router;
 };
+
+
+
+
+
+
