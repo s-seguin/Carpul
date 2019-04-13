3d9785de2200d0d0b6008248be9f72a65975f924
s-seguin <stuart.seguin@ucalgary.ca>
2019-04-13 14:38:06 -0600

    Revert "fixed regsiter sending people to users"
    This reverts commit 08a406e74ad4e586a9a86b8dc66c0d2e644e1fd1.

--- routes/register.js
+++ routes/register.js
@@ -8,7 +8,7 @@ module.exports = function(passport) {
     });
 
     router.post('/', passport.authenticate('local-register', {
-        successRedirect : '/', // redirect to the secure profile section
+        successRedirect : '/users', // redirect to the secure profile section
         failureRedirect : '/register', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
