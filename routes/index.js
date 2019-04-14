ff06caf3c709fb25b0c3d7fcb1454bea7b962758
s-seguin <stuart.seguin@ucalgary.ca>
2019-04-14 16:01:20 -0600

    fixed bug with myRides not displaying correctly

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,8 @@ function insertIntoDB(rideObj) {
 function sendMyRidesToClient(socket){
   let mapObjs = null;
   dbClient.query(
-    "SELECT * FROM ride LEFT OUTER JOIN request ON (ride.ride_id = request.ride_id) LEFT OUTER JOIN account ON (account.user_id = request.user_id) WHERE ride.user_id=$1",[socket.user_id],
+    // "SELECT * FROM ride LEFT OUTER JOIN request ON (ride.ride_id = request.ride_id) LEFT OUTER JOIN account ON (account.user_id = request.user_id) WHERE ride.user_id=$1",[socket.user_id],
+    "SELECT ride.ride_id, ride.user_id, ride.ride_date, ride.start_location, ride.end_location, ride.price_per_seat, ride.available, request.request_id, request.accepted, account.email FROM ride LEFT OUTER JOIN request ON (ride.ride_id = request.ride_id) LEFT OUTER JOIN account ON (account.user_id = request.user_id) WHERE ride.user_id=$1", [socket.user_id],
     (err, res) => {
       if (res) {
         console.log("num rows from getMyRides query " + res.rows.length);
